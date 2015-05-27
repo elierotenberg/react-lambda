f9884fec7f002893b780c830f06392716f94f956
Elie Rotenberg <elie.rotenberg@gmail.com>
2015-05-27 19:35:46 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ export default (React) => {
 
   const wrap = (Wrapper, ...components) => class extends React.Component {
     render() {
-      return <Wrapper {...props}>
+      return <Wrapper {...this.props}>
         {...components}
       </Wrapper>;
     }
@@ -15,7 +15,7 @@ export default (React) => {
 
   const style = (Component, getStyle) => class extends React.Component {
     render() {
-      return <Component {...this.props} {style=(typeof getStyle === 'function' ? getStyle(props) : getStyle)} />;
+      return <Component {...this.props} {style=(typeof getStyle === 'function' ? getStyle(this.props) : getStyle)} />;
     }
   }
 };
